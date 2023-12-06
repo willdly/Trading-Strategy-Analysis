496a2efb1c0981c3bab00c53f36d5e4fac6a3598
Jared Chang <91185297+callforhelp123@users.noreply.github.com>
2023-12-06 15:56:08 -0800

    Delete frontend directory

--- frontend/credentials.py
+++
@@ -1,7 +0,0 @@
-db_credentials = {
-    "username": "capstone2023",
-    "password": "ZsFpNFQ38AKcPMuH",
-    "database": "trading-strategy-analysis",
-    "collection": "Results",
-    "collection2": "processed_results",
-}
